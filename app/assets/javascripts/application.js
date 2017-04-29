8fa82c0e03f28754841b76500e609e881d093b2f
ka <ka.kaosf@gmail.com>
2017-04-29 16:10:00 +0900

    Enable jQuery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,9 @@
 //
 //= require rails-ujs
 //= require turbolinks
+//
+//= require jquery
+//= require jquery_ujs
+// ref. https://github.com/rails/jquery-rails#installation
+//
 //= require_tree .
